da61fdeed0303ab0a31d5d280c70a985936d9f09
MARUNGs <mi1764@naver.com>
2022-08-10 15:05:07 +0900

    Vue3 환경설정 및 실행까지만...

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <h4>XX 원룸</h4>
+      <p>{{  }}</p>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+// 일반 Vue3 환경
 
-export default {
-  name: 'App',
+import { Options, Vue } from 'vue-class-component';
+
+let 이름:string = 'kim' // ts: 변수 자료형 지정 가능
+
+@Options({
+  data() {
+    return {
+      // 데이터 담기
+      price1: 60,
+      name: 이름
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
