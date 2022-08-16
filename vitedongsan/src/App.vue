9a05304670f05f1e3296033ba688a09ad4f7aac0
MARUNGs <mi1764@naver.com>
2022-08-16 18:15:05 +0900

    Vue3 import / export

--- vitedongsan/src/App.vue
+++ vitedongsan/src/App.vue
@@ -1,96 +1,143 @@
 <template>
+    <!-- UI 생성법 : 미리 만들어두고 조건이 맞으면 보여주는 것! 
+        1. UI의 현재 상태를 데이터로 저장
+        2. 데이터에 따라 UI 보이기 설정
+    -->
+    <div class="black-bg" v-if="dataObj.modalFlag">
+      <div class="white-bg">
+        <span class="right" @click="() => {
+          dataObj.modalFlag = false
+        }"> X </span>
+        <!-- <b-button variant="outline-primary" class="right" @click="() => { dataObj.modalFlag = false }"> X </b-button> -->
+        <h4>상세페이지 title</h4>
+        <p>상세페이지 content</p>
+      </div>
+    </div>
+
   <div>
-    <!-- HTML 안에서 반복문 : v-for -->
-    <!-- 연습 필요 ... -->
-    <!-- <div class="menu" v-for="(data, i) in dataObj" :key="i">
-      {
-        (data)  
-      }
-      <a v-for="(menus, j) in data" :key="j"> {{ }}</a>
-      <a>1</a>
-      <a>2</a>
-      <a>3</a>
-    </div> -->
-
     <div class="menu">
-      <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+      <a v-for="(menu, i) in dataObj.menus" :key="i">{{ menu }}</a>
     </div>
+  </div>
 
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-
-    <div v-for="(product, i) in products" :key="product">
-      <h4>{{ product }}</h4>
-      <p>{{ 100 + i }}원</p>
-      <button v-on:click="++count">허위매물 신고  <span>신고 수 : {{ count }}</span> </button>
-    </div>
+  <div v-for="(data, i) in dataObj.oneRoomData" class="content">
+    <img :src="data.image" class="room-img" />
+    <h4 :style=dataObj.style @click="modalOpen()">{{ data.title }}</h4>
+    <p>{{ data.content }}</p>
+    <p>{{ data.price }}원</p>
+    <!-- <button @click="increase(i)">허위매물 신고바람!</button>
+    <span>신고 수 : {{ dataObj.countList[i].count  }}</span> -->
   </div>
 </template>
 
 <!-- ------------------------------------------------------------------------------------------------------------------ -->
 
-<!-- Vue 3버전 -->
+<!-- Vue 3 -->
 <script setup lang="ts">
 import {ref, reactive} from "@vue/reactivity";
 // 외부 타입스크립트 객체를 불러와야한다.
-import {AppInfo} from "@/types"
+import {AppInfo, Data} from "@/types"
 
-// 객체형 외부 타입스크립트
-// const obj = reactive<{알수없음?: number, roomName: string, menus: string[], count: number}>({
+// 2. 객체형 외부 타입스크립트
 const dataObj = reactive<AppInfo>({
-  roomName: '가나다라',
-  style: 'color: skyblue',
-  products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
-  menus: ['Home', 'shop', 'abount'],
-  count: 0
+  style: 'color: skyblue; cursor: pointer;',
+  products: [{ id: 0, name: '역삼동 원룸' }, { id: 1, name: '천호동 원룸' }, { id: 2, name: '마포구 원룸' }],
+  menus: ['Home', 'shop', 'about'],
+  countList: [{ id: 0, count: 0 }, { id: 1, count: 0 }, { id: 2, count: 0 }],
+  modalFlag: false,
+  oneRoomData: Data
 })
 
-// console.log(dataObj.menus)
-// console.log(Object.keys(dataObj))
+// 함수 생성
+function increase(i :number) :void {
+  let products = dataObj.products
+  let countList = dataObj.countList
 
-// 단순 자료형 타입스크립트
+  if(products && countList) {
+    if(products[i].id === countList[i].id) {
+      ++countList[i].count
+    } else {
+      console.log('id가 없드')
+    }
+  } else {
+    console.log('객체가 없드')
+  }
+}
+
+// 모달창 오픈
+function modalOpen() :void {
+  let flag = dataObj.modalFlag
+
+  if(!flag) {
+    dataObj.modalFlag = true
+  } else {
+    dataObj.modalFlag = false
+  }
+}
+
+// 1. 단순 자료형 타입스크립트
 // let roomName = ref<string>('허위매물')
 // let style    = ref<string>('color: skyblue')
-let products = ref<string[]>(['역삼동 원룸', '천호동 원룸', '마포구 원룸'])
-let menus    = ref<string[]>(['Home', 'shop', 'abount'])
-let count    = ref<number>(0)
+
+// let products = ref<string[]>(['역삼동 원룸', '천호동 원룸', '마포구 원룸'])
+// let menus    = ref<string[]>(['Home', 'shop', 'abount'])
+// let count    = ref<number>(0)
+
 // console.log(roomName.value) // ref는 .value로 접근해야 값을 얻을 수 있다.
 </script>
 
 
+<!-- -------------------------------------------------------------------------------------------------------------- -->
 
-<!-- Vue 2버전 -->
-<!-- 
-<script>
+
+<!-- Vue 2 -->
+
+<!-- <script>
 export default {
   name: 'App',
   data() {
     return {
       style: 'color: blue',
-      products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
+      products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      menus: ['Home', 'shop', 'about'],
+      count: 0,
+      modalFlag: false // 모달 상태값 <<<--- state 라고 부름. 상태값 취급!
+    }
+  },
+  methods: { // 함수 정의 공간
+    increase() { // 함수 안에서 데이터 사용 시 this 이용.
+      ++this.count;
     }
   },
   components: {
 
   }
 }
-</script> 
--->
+</script>  -->
+
 
 <style scoped>
+/* 기본 설정 */
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
+/* 내비게이션 설정 */
 .menu {
   background: darkmagenta;
   padding:15px;
+  width: 100%;
   border-radius: 5px;
 }
 .menu a {
   color: white;
   padding: 10px;
 }
+
+/* logo 설정 */
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -101,5 +148,35 @@ export default {
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+/* v-for */
+.content {
+  width: 33%;
+  float: left;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+/* modal 설정 */
+.black-bg {
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 10px;
+  padding: 20px;
+}
+.right {
+  float: right;
+  cursor: pointer;
 }
 </style>
--- vitedongsan/src/types.ts
+++ vitedongsan/src/types.ts
@@ -1,12 +1,67 @@
+// 객체 설정
 export interface AppInfo {
   /* 원룸 이름 */
-  roomName?: string
+  roomName? :string
   /* css style */
-  style?: string
+  style? :string
   /* 상품목록 list */
-  products?: string[]
+  products :{id: number, name :string}[]
   /* 내비게이션 */
-  menus?: string[]
+  menus :string[]
   /* 신고 수 */
-  count?: number
+  countList :{id :number, count :number}[]
+  /* 모달창 상태값 */
+  modalFlag :boolean
+  /* 원룸 데이터 */
+  oneRoomData : {
+    id :number,
+    title :string,
+    image :string,
+    content :string,
+    price :number
+  }[]
 }
+
+// 원룸 실제 데이터 
+export let Data = [{
+  id : 0,
+  title: "Sinrim station 30 meters away",
+  image: "https://codingapple1.github.io/vue/room0.jpg",
+  content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
+  price: 340000
+  },
+  {
+  id : 1,
+  title: "Changdong Aurora Bedroom(Queen-size)",
+  image: "https://codingapple1.github.io/vue/room1.jpg",
+  content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
+  price: 450000
+  },
+  {
+  id : 2,
+  title: "Geumsan Apartment Flat",
+  image: "https://codingapple1.github.io/vue/room2.jpg",
+  content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
+  price: 780000
+  },
+  {
+  id : 3,
+  title: "Double styled beds Studio Apt",
+  image: "https://codingapple1.github.io/vue/room3.jpg",
+  content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
+  price: 550000
+  },
+  {
+  id : 4,
+  title: "MyeongIl Apartment flat",
+  image: "https://codingapple1.github.io/vue/room4.jpg",
+  content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
+  price: 680000
+  },
+  {
+  id : 5,
+  title: "Banziha One Room",
+  image: "https://codingapple1.github.io/vue/room5.jpg",
+  content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
+  price: 370000
+}];
