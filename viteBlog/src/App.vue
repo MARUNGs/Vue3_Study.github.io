639e86e8002decf2a28e61a03210fb8ca8c33810
MARUNGs <mi1764@naver.com>
2022-08-23 02:11:49 +0000

    Create List.vue

--- viteBlog/src/App.vue
+++ viteBlog/src/App.vue
@@ -1,7 +1,7 @@
-<template>
+<<template>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">ViteBlog</a>
     <button class="navbar-toggler" @click="onClick" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,6 +35,14 @@
     </div>
   </div>
 </nav>
+
+<di class="container mt-4"v>
+  h5>Vite Blog</h5>
+  <p>- Vite + Vue 3.2 + TypeScript -</p>
+</div>
+
+
+
 </template>
 
 <script setup lang="ts">
